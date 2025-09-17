d4b067b7a68ef2a0d1f875ed044616104949e5fe
TorresTheo <uapv2103150@e-mc128-14.ad.univ-avignon.fr>
2025-09-17 12:33:22 +0200

    push

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -222,7 +222,7 @@ class ListeSimpleTest {
 
     @Test
     void supprimeTousListeVide() {
-        listeATester.supprimeTous();
+        listeATester.supprimeTous(listeATester.getSize());
         assertNull(listeATester.tete);
         assertEquals(0, listeATester.getSize());
     }
